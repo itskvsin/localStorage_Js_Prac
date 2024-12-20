6eec58eacb4f97616a0876a100b175f444ba3ad4
Kevin <kevinsolankistudy@gmail.com>
2024-12-20 15:20:25 +0530

    Created the todo(demo) UI

---
+++ script.js
@@ -0,0 +1,11 @@
+// const userArray = ["Kevin" , 18]
+
+// localStorage.setItem("user" , JSON.stringify(userArray))
+
+// const showUser = JSON.parse(localStorage.getItem("user"))
+
+// console.log(showUser)
+
+// localStorage.removeItem("user")
+
+// localStorage.clear()
